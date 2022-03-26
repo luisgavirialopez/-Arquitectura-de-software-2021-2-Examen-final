0c6197c734ab21bdbef6100e72d3b80d04fe4e0b
luisgavirialopez <87778291+luisgavirialopez@users.noreply.github.com>
2022-03-26 10:49:46 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Enlace donde obtuve información para la realización
+
+https://www.delftstack.com/es/howto/java/xml-to-json-java/#:~:text=La%20cadena%20XML%20anterior%20se,XML%2C%20en%20este%20caso%20examplexml%20.
+
---
+++ Usuario.java
@@ -0,0 +1,40 @@
+package xmlajson_json_simple;
+
+
+public class Usuario {
+    
+    String id;
+    String nombre;
+    String telefono;
+
+    public Usuario(String id, String nombre, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
